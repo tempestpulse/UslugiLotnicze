c72f03a3d4a6ea0fe890fb6544a608263427325b
tempestpulse <bulsar@tutanota.com>
2023-06-15 21:24:07 +0200

    pierwsza wersja

--- UslugiLotnicze/Lot.cs
+++ UslugiLotnicze/Lot.cs
@@ -4,14 +4,14 @@ class Lot
 {
     public Samolot Samolot { get; }
     public Trasa Trasa { get; }
-    public DateTime DepartureTime { get; }
-    public DateTime ArrivalTime { get; }
+    public DateTime CzasWylotu { get; }
+    public DateTime CzasPrzylotu { get; }
 
     public Lot(Samolot samolot, Trasa trasa, DateTime czasWylotu, DateTime czasPrzylotu)
     {
         Samolot = samolot;
         Trasa = trasa;
-        DepartureTime = czasWylotu;
-        ArrivalTime = czasPrzylotu;
+        CzasWylotu = czasWylotu;
+        CzasPrzylotu = czasPrzylotu;
     }
 }
